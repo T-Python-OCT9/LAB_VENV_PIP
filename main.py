a8dcfc7881d828632152d44192b6386eaa73f3a5
Asrar alrashed <asraralrashed2@gmail.com>
2022-10-25 18:11:24 +0300

    LAB_VENV_PIP

---
+++ main.py
@@ -0,0 +1,21 @@
+from art import*
+
+
+Art=text2art("ART")
+print(Art)
+
+print( text2art("BELIEVE"+ 
+"ACHEIVE", font='block', chr_ignore=True ))
+ 
+print(text2art(''' test ''', font="small"))
+
+
+print(text2art ("test ","whith_bubble"))
+
+# tprint('''Lorem  
+# ipsum 
+# dolor''', font="cybermedium")
+
+tprint("HII","rnd-xlarge")
+
+tprint("test","rnd-xlarge")
