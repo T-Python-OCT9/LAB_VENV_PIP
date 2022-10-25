816306869725c992a19adb3b22dcea78bc53be81
Samaa <Samaanawaf@gmail.com>
2022-10-25 17:46:31 +0300

    lab venv pip

---
+++ main.py
@@ -0,0 +1,7 @@
+from art import tprint
+
+tprint("BELIEVE",font="block",chr_ignore=True)
+tprint("And",font="block",chr_ignore=True)
+tprint("ACHEIVE",font="block",chr_ignore=True)
+
+tprint("Samaa",font="cybermedium")
